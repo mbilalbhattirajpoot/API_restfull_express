978cb3c12ee1867829c77ffe6eee087eedc82868
mbilalbhattirajpoot <m.bilalbhattirajpoot@gmail.com>
2020-12-19 17:43:45 -0800

    #5 curl req

--- index.js
+++ index.js
@@ -11,3 +11,9 @@ app.get('/', (req, res) => {
 })
 // Starting server
 app.listen('1337');
+
+const app = express()
+app.use(morgan('tiny'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(require('./routes/index.routes'))
