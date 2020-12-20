e4e3bf70ca48c5b3376421d25f253d759041e87e
mbilalbhattirajpoot <m.bilalbhattirajpoot@gmail.com>
2020-12-19 17:42:15 -0800

    #4 jason data

---
+++ routes/post.routes.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const router = express.Router()
+const post = require('../models/post.model')
+const m = require('../helpers/middlewares')
+// Routes
+module.exports = router
+
+/* All posts */
+router.get('/', async (req, res) => {
+    await post.getPosts()
+        .then(posts => res.json(posts))
+        .catch(err => {
+            if (err.status) {
+                res.status(err.status).json({ message: err.message })
+            } else {
+                res.status(500).json({ message: err.message })
+            }
+        })
+})
