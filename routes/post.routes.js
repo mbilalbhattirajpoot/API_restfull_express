283b5a5d44c639f47bd671b38a534a5d28a56e69
mbilalbhattirajpoot <m.bilalbhattirajpoot@gmail.com>
2020-12-22 13:59:25 -0800

    #7 requests

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -31,3 +31,45 @@ router.get('/:id', m.mustBeInteger, async (req, res) => {
         }
     })
 })
+
+/* Insert a new post */
+router.post('/', m.checkFieldsPost, async (req, res) => {
+    await post.insertPost(req.body)
+    .then(post => res.status(201).json({
+        message: `The post #${post.id} has been created`,
+        content: post
+    }))
+    .catch(err => res.status(500).json({ message: err.message }))
+})
+
+/* Update a post */
+router.put('/:id', m.mustBeInteger, m.checkFieldsPost, async (req, res) => {
+    const id = req.params.id
+    await post.updatePost(id, req.body)
+    .then(post => res.json({
+        message: `The post #${id} has been updated`,
+        content: post
+    }))
+    .catch(err => {
+        if (err.status) {
+            res.status(err.status).json({ message: err.message })
+        }
+        res.status(500).json({ message: err.message })
+    })
+})
+
+/* Delete a post */
+router.delete('/:id', m.mustBeInteger, async (req, res) => {
+    const id = req.params.id
+    
+    await post.deletePost(id)
+    .then(post => res.json({
+        message: `The post #${id} has been deleted`
+    }))
+    .catch(err => {
+        if (err.status) {
+            res.status(err.status).json({ message: err.message })
+        }
+        res.status(500).json({ message: err.message })
+    })
+})
